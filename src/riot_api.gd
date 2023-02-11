90a7694f880d78b903dd63df7e0442dc3b048be5
mihaigabriel16 <mihai.gabriel1608@gmail.com>
2023-02-11 20:03:53 +0200

    update work

--- src/riot_api.gd
+++ src/riot_api.gd
@@ -1,75 +1,58 @@
 extends Node
 
-var http_request = HTTPRequest.new()
-
-var key = "RGAPI-2795fc01-4389-4a92-a7e6-54b97ab0be6d"
+var key = ""
 var apiVersion = null
-var last_request = null
-var REQUEST_QUEUE = []
-var IS_REQUESTING = false
 
 var API_URL = "https://%s.api.riotgames.com"
 var ENDPOINT_PLATFORM = "/lol/platform/v%s/"
 var ENDPOINT_SUMMONER = "/lol/summoner/v%s/"
 
-var PlatformsDictionary = {
-	"EUNE": "eun1",
-	"EUW": "euw1",
-	"NA": "na1"
-}
+
 
 func _ready():
-	add_child(http_request)
-	http_request.connect("request_completed", self, "_http_request_completed")
-	GetVersion()
-	$OptionButton.add_item("EUW")
-	$OptionButton.add_item("EUNE")
+	var test = GetVersion()
 	
-	
-func request(function):
-	call(function)
-	
-func _http_request_completed(result, response_code, headers, body):
-	print("Response Code: "+str(response_code)+"\nResult: "+str(result))
-	match last_request:
-		"GetVersion":
-			apiVersion = parse_json(body.get_string_from_utf8())[0]
-			print("API VERSION: " + str(apiVersion))
-		"GetSummonerByName":
-			var data = parse_json(body.get_string_from_ascii())
-			print(data)
-	#+"\nBody: "+body.get_string_from_utf8())
 
-func GetPlatform(region):
-	return PlatformsDictionary[region]
-
-func GetSummonerByName(name, platform):
-	last_request = "GetSummonerByName"
+	
+func GetSummonerByName(name: String):
+	var http = HTTPRequest.new()
+	add_child(http)
 	var api_call: String = "summoners/by-name/" + name
 	var raw: String = API_URL + ENDPOINT_SUMMONER + api_call
-	var url: String = raw % ["eun1", platform]
+	var url: String = raw % ["eun1", 4]
 	print(url)
 	var token = "X-Riot-Token:" + key
 	var headers = [token]
-	http_request.request(url, headers, false)
+	var req = http.request(url, headers, false)
+	var task = yield(http, "request_completed")
+	var data = task[3].get_string_from_utf8()
+	var json = JSON.parse(data).result
+	return json
 
-func GetSummonerByAccount(name, platform):
-	last_request = "GetSummonerByName"
-	var api_call: String = "summoners/by-name/" + name
+
+func GetSummonerByAccount(encryptedAccountId: String):
+	var http = HTTPRequest.new()
+	add_child(http)
+	var api_call: String = "summoners/by-account/" + encryptedAccountId
 	var raw: String = API_URL + ENDPOINT_SUMMONER + api_call
-	var url: String = raw % ["euw1", platform]
+	var url: String = raw % ["eun1", 4]
 	print(url)
 	var token = "X-Riot-Token:" + key
 	var headers = [token]
-	http_request.request(url, headers, false)
+	http.request(url, headers, false)
+	
 
 func GetVersion():
-	last_request = "GetVersion"
+	var http = HTTPRequest.new()
+	add_child(http)
 	var url = "https://ddragon.leagueoflegends.com/api/versions.json"
-	var req = http_request.request(url)
+	var req = http.request(url)
+	var task = yield(http, "request_completed")
+	var data = task[3].get_string_from_utf8()
+	var json = JSON.parse(data).result[0]
+	return json
 
 
 func _on_Button_pressed():
-	var region = GetPlatform($OptionButton.get_item_text($OptionButton.get_selected_id()))
 	#print(region)s
-	GetSummonerByName($TextEdit.text, 4)
+	GetSummonerByName($TextEdit.text)
